7ba92a8241e4d4a90e30599c2e690bd2bb38ec2f
Jon Brown <jonbrowndeveloper@gmail.com>
2018-02-14 22:15:00 -0600

    added some layout and a main image

--- index.php
+++ index.php
@@ -11,7 +11,23 @@
 
 <body>
 
+<h1>HangMan</h1>
 
+<img src='/img/hangman.png' alt='picture of a hangman'>
+
+<form>
+
+    <input type='radio' name='easy' value='easy' checked> Easy<br>
+    <input type='radio' name='medium' value='medium'> Medium<br>
+    <input type='radio' name='hard' value='hard'> Hard<br>
+
+</form>
+
+<form method='GET' action='game.php' width='400' height='600'>
+
+    <input type='submit' value='Begin Game!'>
+
+</form>
 
 </body>
 
