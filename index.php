9958f7d06d8a85dfcf3addfa37ac7c5fe8f9730e
Jon Brown <jonbrowndeveloper@gmail.com>
2018-02-14 21:34:43 -0600

    initial commit with html template and bootstrap

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Jon Brown</title>
+    <meta charset="utf-8">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
