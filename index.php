c600b0cc89419ffa37527f4d4a7cd2d0da8f46a6
Jon Brown <jonbrowndeveloper@gmail.com>
2018-02-20 20:56:44 -0600

    changed my idea to a word prioritization sorter

--- index.php
+++ index.php
@@ -2,32 +2,94 @@
 <html>
 <head>
 
-    <title>Jon Brown</title>
+    <title>Word Perspective</title>
     <meta charset="utf-8">
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+
+    <link href="/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
 </head>
 
 <body>
 
-<h1>HangMan</h1>
+<div class='container-fluid'>
 
-<img src='/img/hangman.png' alt='picture of a hangman'>
+    <h1 class='text-center'>Word Perspective!</h1>
 
-<form>
+    <h4 class='text-center'>show the most important words</h4>
 
-    <input type='radio' name='easy' value='easy' checked> Easy<br>
-    <input type='radio' name='medium' value='medium'> Medium<br>
-    <input type='radio' name='hard' value='hard'> Hard<br>
+</div>
 
-</form>
+<div class='container'>
+    <div class='row'>
+        <div class='col'>
 
-<form method='GET' action='game.php' width='400' height='600'>
+            <form>
 
-    <input type='submit' value='Begin Game!'>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="optionRadios" id="optionRadios1" value="option1" checked>
+                    <label class="form-check-label" for="optionRadios1">
+                        Show my text in its default order
+                    </label>
+                </div>
 
-</form>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="optionRadios" id="optionRadios1" value="option2">
+                    <label class="form-check-label" for="optionRadios1">
+                        Show my text in alphabetical order
+                    </label>
+                </div>
+
+            </form>
+
+            <br>
+
+            <form>
+                <div class='form-group'>
+                    <button type="submit" class="btn btn-primary">Go!</button>
+                </div>
+            </form>
+
+        </div>
+        <div class='col'>
+
+            <div class="form-group">
+                <label for="outputTextArea">Output</label>
+                <textarea class="form-control" id="outputTextArea" rows="16"></textarea>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            1 of 3
+        </div>
+        <div class="col-6">
+            2 of 3 (wider)
+        </div>
+        <div class="col">
+            3 of 3
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            1 of 3
+        </div>
+        <div class="col-5">
+            2 of 3 (wider)
+        </div>
+        <div class="col">
+            3 of 3
+        </div>
+    </div>
+</div>
+
 
 </body>
 
